c832c034a8b47dc00b333842bd27894a9e9019f1
sathvik125 <98334405+sathvik125@users.noreply.github.com>
2024-03-15 23:50:59 +0530

    Update Firebase.js

--- src/Firebase.js
+++ src/Firebase.js
@@ -8,13 +8,7 @@ import { getFirestore } from "firebase/firestore";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyBxRw8DAoeAzEhZNm2ft41nGrYvJgE_WFM",
-  authDomain: "shoppingapp-fb6b1.firebaseapp.com",
-  projectId: "shoppingapp-fb6b1",
-  storageBucket: "shoppingapp-fb6b1.appspot.com",
-  messagingSenderId: "445591605343",
-  appId: "1:445591605343:web:245e290354971c0319cbea",
-  measurementId: "G-48Y588KB3T"
+ 
 };
 
 // Initialize Firebase
